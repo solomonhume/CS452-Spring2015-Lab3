c6aabe4c7e559f8dacb36cfed7f0c5f4470e7bb1
solomonhume <solomonhume@gmail.com>
2015-03-02 21:34:55 -0500

    Update lab3.js

--- lab3.js
+++ lab3.js
@@ -1,22 +1,164 @@
+// Jiaju Huang 0530537
+// jiajhua@clarkson.edu
+var canvas;
 var gl;
-var points;
+
+var numVertices  = 36;
+
+var axis = 0;
+var xAxis = 0;
+var yAxis =1;
+var zAxis = 2;
+var theta = [ 0, 0, 0 ];
+var thetaLoc;
+
+    var vertices = [
+        vec3( -0.5, -0.5,  0.5 ),
+        vec3( -0.5,  0.5,  0.5 ),
+        vec3(  0.5,  0.5,  0.5 ),
+        vec3(  0.5, -0.5,  0.5 ),
+        vec3( -0.5, -0.2, -0.5 ),
+        vec3( -0.5,  0.5, -0.5 ),
+        vec3(  0.5,  0.3, -0.5 ),
+        vec3(  0.5, -0.5, -0.2 )
+    ];
+
+    var vertexColors = [
+        vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+        vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+        vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+        vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+        vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+        vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+    ];
+
+// indices of the 12 triangles that compise the cube
+
+var indices = [
+    1, 0, 3,
+    3, 5, 1,
+    2, 3, 6,
+    7, 6, 2,
+    3, 0, 5,
+    4, 7, 3,
+    6, 5, 1,
+    1, 2, 6,
+    4, 5, 6,
+    6, 7, 4,
+    5, 4, 0,
+    0, 1, 7
+];
+
+speed = 0;
+
+function doKeyDown(e) {
+
+    switch (e.which) {
+     case 97:
+     {axis = xAxis; // shift and left arrow key
+     	 speed = 1;}
+        break;
+     case 100:
+     {axis = xAxis; //up arrow key
+     	 speed = -1;}
+        break;
+     case 119:
+     {	
+     	{axis = yAxis;
+     		speed = 1;}
+     	break;}
+     case 115:
+     	{
+     	{axis = yAxis;
+     		speed = -1;}
+     	break;}
+
+    }
+    //alert(vertices[1]);
+
+    
+    // Initialize event handlers
+
+    render();
+
+}
+
+
 window.onload = function init()
 {
-var canvas = document.getElementById( "gl-canvas" );
-gl = WebGLUtils.setupWebGL( canvas );
-if ( !gl ) { alert( "WebGL isn't available" ); }
-//
-// Configure WebGL
-//
-gl.viewport( 0, 0, canvas.width, canvas.height );
-gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-// Load shaders and initialize attribute buffers
-var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-gl.useProgram( program );
-// Load the data into the GPU
-// Associate our shader variables with our data buffer
-render();
-};
-function render() {
-gl.clear( gl.COLOR_BUFFER_BIT );
+    canvas = document.getElementById( "gl-canvas" );
+    window.addEventListener( "keypress", doKeyDown, false );
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    gl.enable(gl.DEPTH_TEST);;
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // array element buffer
+    
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+    
+    // color array atrribute buffer
+    
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    // vertex array attribute buffer
+    
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    thetaLoc = gl.getUniformLocation(program, "theta"); 
+    
+    //event listeners for buttons
+    
+    document.getElementById( "xButton" ).onclick = function () {
+        axis = xAxis;
+    };
+    document.getElementById( "yButton" ).onclick = function () {
+        axis = yAxis;
+    };
+    document.getElementById( "zButton" ).onclick = function () {
+        axis = zAxis;
+    };
+
+    
+    render();
 }
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    theta[axis] += speed;
+    gl.uniform3fv(thetaLoc, theta);
+
+
+    gl.drawElements( gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0 );
+
+    requestAnimFrame( render );
+}
+
+
